b7be2317d0738bc99e73178d5d45dbe5b69ed2b8
unknown <C:\Users\7-2-16\AppData\Roaming\The Bat!>
2019-03-05 14:49:17 +0500

    Чат доведен до ума

--- js/chat-slide.js
+++ js/chat-slide.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
-	
+	//скрытие чата
+	$('.show-btn').click(function(hideChat){
+		hideChat.preventDefault();
+		$('.show-btn').removeClass('close').addClass('active');
+		$('.chat-footer').slideDown().removeClass('close').addClass('active');
+	});
+
+	$('.hide-btn').click(function(hideChat){
+		$('.show-btn').removeClass('active').addClass('show-btn');
+		$('.chat-footer.active').slideUp().removeClass('active').addClass('close');
+	});
+
+
 
 });
